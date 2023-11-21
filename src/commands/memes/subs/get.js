15633241d613221318abd3592d433efa7c136ce6
Luis Henrique <lhsatsuma@gmail.com>
2023-11-21 18:43:55 -0300

    v1.4.1b-002 - memes:rand changed to show on reply.

--- src/commands/memes/subs/get.js
+++ src/commands/memes/subs/get.js
@@ -31,10 +31,7 @@ module.exports = {
             .setColor(getUtils().getColor('BLUE'))
             .setImage(bean.url);
 
-        await interaction.deferReply();
-        await interaction.deleteReply();
-
-        await interaction.channel.send({
+        await interaction.reply({
             content: `[${bean.order_id}] ${bean.name}`,
             embeds: [embedMsg]
         });
